77b715e0f1070b7e15922293e943c70e06bf22e0
Seu Nome <seu-email@example.com>
2025-03-16 09:01:48 -0300

    adicionado bcrypto

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,14 @@
 import fastify from "fastify";
 import { z } from "zod";
 import { prisma } from "./lib/prisma";
+import { register } from "./http/controllers/register";
+import { appRoutes } from "./http/routes";
 
 
 export const app = fastify();
 
+app.register(appRoutes);
 
 
-app.post("/users", async (req, rep) => {
-  const registerSchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-  });
 
-  const { name, email, password } = registerSchema.parse(req.body);
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  });
-
-  return rep.status(201).send({ message: "Seu usuario foi criado com sucesso!!!" });
-});
